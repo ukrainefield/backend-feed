35124ac0e65450d68c70761fe46807a199d0dcc4
Jaap <33700526+GewoonJaap@users.noreply.github.com>
2022-03-02 16:54:21 +0100

    Add cache headers

--- routes/feed.js
+++ routes/feed.js
@@ -7,14 +7,26 @@ const cache = new NodeCache({ stdTTL: consts.Cache.Cache_TTL });
 
 /* GET home page. */
 router.get("/all", async function (req, res, next) {
-  if (await cache.get(req.originalUrl.toLowerCase())) {
+  const cacheKey = req.originalUrl.toLowerCase();
+  const cachedResponse = await cache.get(cacheKey);
+  if (cachedResponse) {
     console.log("Cached all feed");
-    return res.status(200).json(cache.get(req.originalUrl.toLowerCase()));
+    const cacheTTL = cache.getTtl(cacheKey);
+
+    res.setHeader(consts.Headers.wasCached, 1);
+    res.setHeader(consts.Headers.cacheTTL, consts.Cache.Cache_TTL);
+    res.setHeader(
+      consts.Headers.cachedAtTime,
+      cacheTTL - consts.Cache.Cache_TTL * 1000
+    );
+    res.setHeader(consts.Headers.expiresAtTime, cacheTTL);
+
+    return res.status(200).json(cachedResponse);
   }
 
   try {
     const data = await feedGenerator.generateFeed();
-    cache.set(req.originalUrl.toLowerCase(), data, consts.Cache.Cache_TTL);
+    cache.set(cacheKey, data, consts.Cache.Cache_TTL);
     return res.status(200).json(data);
   } catch (e) {
     return res.status(500).json({ message: e.message });
